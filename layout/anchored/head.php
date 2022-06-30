945e438d24954c3f22f7a1bb7806384c49e7d46c
P. Mergey <gzm@gizmecano.net>
2022-06-30 13:40:48 +0200

    Displace `<base>` element before `<link>` elements

--- layout/anchored/head.php
+++ layout/anchored/head.php
@@ -1,9 +1,9 @@
 <head>
-  <base href="/" />
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title><?php echo $meta['title']; ?></title>
   <meta name="description" content="<?php echo $meta['blurb']; ?>">
+  <base href="/" />
   <link rel="canonical" href="<?php echo $meta['canon']; ?>">
   <link rel="stylesheet" href="design/assets/inter/inter.css" media="all">
   <link rel="stylesheet" href="design/assets/fraunces/fraunces.css" media="all">
