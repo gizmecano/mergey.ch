01db4a56d844971df1fb55882ca59db8628cc2dc
P. Mergey <gzm@gizmecano.net>
2022-07-03 10:35:48 +0200

    Reorganize and clean file for requisite variables

--- layout/required/variables.php
+++ layout/required/variables.php
@@ -1,33 +1,29 @@
 <?php
 // Get current active host path
-
 $site['host'] = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
 
 // Decode dependency management setup
-
 $composer = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/composer.json'), true);
 
-// Set language variables for site and page
-
+// Set language variables for site
 $site['lang'] = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 if ($site['lang'] != 'fr') {
   $site['lang'] = 'en';
 }
+
+// Prepare variables based on decoded data
+$site['name'] = ucfirst(substr($composer['name'], -9));
+$site['nick'] = substr($composer['name'], 0, 9);
+
+// Set language variables for page
 if (empty($page['lang'])) {
   $page['lang'] = $site['lang'];
 }
 
-// Decode requiste languages glossary based on language
-
+// Decode requisite glossary based on language variable
 $glossary = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/record/content/' . $page['lang'] . '/glossary.json'), true);
 
-// Prepare variables based on decoded data
-
-$site['name'] = ucfirst(substr($composer['name'], -9));
-$site['nick'] = substr($composer['name'], 0, 9);
-
 // Define page title based on page body
-
 switch ($page['body']) {
   case 'index':
     $meta['title'] = $site['name'];
@@ -36,8 +32,7 @@ switch ($page['body']) {
     $meta['title'] = $page['name'] . ' - ' . $site['name'];
 }
 
-// Define page description based on page type
-
+// Define page description based on page body
 switch ($page['body']) {
   case 'index':
     $meta['blurb'] = $site['name'] . ' — ' . ucfirst($glossary['site']['motto']);
@@ -46,8 +41,7 @@ switch ($page['body']) {
     $meta['blurb'] = $site['name'];
 }
 
-// Define canonical link based on page type
-
+// Define canonical link based on page body
 switch ($page['body']) {
   case 'index':
     $meta['canon'] = $site['host'];
